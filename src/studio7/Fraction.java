525fd00dde860f2f7d6a68dae595e554032547bc
gsfx1 <gsfx1@BOOK-Q8E8M7NDHH.dhcp.wustl.edu>
2024-10-31 11:20:54 -0500

    1031

--- src/studio7/Fraction.java
+++ src/studio7/Fraction.java
@@ -1,7 +1,24 @@
 package studio7;
 
 public class Fraction {
+private int numerator;
+private int denominator;
 
+public Fraction(int top, int bottom) {
+	numerator = top;
+	denominator = bottom;
+}
+
+public int getNumerator() {
+	return numerator;
+}
+public int getDenominator() {
+	return denominator;
+}
+
+public int Multiplication(int numerator, int denominator) {
+	int newnumer
+}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
